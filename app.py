c710361c82a2926c23d4c422dcddd38e817fdb69
joys021 <joys021@gmail.com>
2019-05-18 21:07:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask,render_template, jsonify
 from flask_bootstrap import Bootstrap
 from flask_marshmallow import Marshmallow
@@ -15,7 +14,7 @@ ma = Marshmallow(app)
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ("bucket_name", "key", "last_modified")
+        fields = ("bucket_name", "key", "last_modified","size")
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
@@ -34,8 +33,25 @@ def files():
     summaries = my_bucket.objects.all()
     result = users_schema.dump(summaries)
     return jsonify(result.data)
+    #return jsonify({'in':'progress'})
     #return render_template('files.html', my_bucket=my_bucket, files=summaries)
     
 
+class SSeSchema(ma.Schema):
+    class Meta:
+        fields = ("bucket_name", "key", "last_modified", "server_side_encryption", "sse_customer_algorithm", "sse_customer_key_md5", "ssekms_key_id")
+
+sse_schema = SSeSchema()
+sses_schema = SSeSchema(many=True)
+
+
+@app.route('/obj')
+def objj():
+    s3 = boto3.resource('s3')
+    obj = s3.Object(S3_BUCKET,'Hello_flask.py')
+    result = sse_schema.dump(obj)
+    return jsonify(result.data)
+
 if __name__ == '__main__':
     app.run()
+    
