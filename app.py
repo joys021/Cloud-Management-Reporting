675da26bc934a7447542271b798043079713d5b3
joys021 <joys021@gmail.com>
2019-06-02 18:08:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,8 @@ Bootstrap(app)
 def index():
     return render_template('index.html') 
 
+
+#display the details of the files in a bucket
 @app.route('/files')
 def files():
     s3_resource = boto3.resource('s3')
@@ -48,8 +50,9 @@ sse_schema = SSeSchema()
 sses_schema = SSeSchema(many=True)
 
 
-@app.route('/obj')
-def objj():
+#display the details of a file in a particular bucket including encryption details
+@app.route('/objectdetails')
+def objectdetails():
     s3 = boto3.resource('s3')
     obj = s3.Object(S3_BUCKET,'Hello_flask.py')
     result = sse_schema.dump(obj)
@@ -63,6 +66,7 @@ class BucSchema(ma.Schema):
 buc_schema = BucSchema()
 bucc_schema = BucSchema(many=True)
 
+#display the list of buckets 
 @app.route('/buckets')
 def buckets():
     client = boto3.client('s3')
@@ -71,9 +75,9 @@ def buckets():
     return jsonify(result.data)
     
     
-    
-@app.route('/buc')
-def bucc():
+#create new bucket    
+@app.route('/createbucket')
+def createbucket():
     s3 = boto3.resource('s3')
     bucket = s3.create_bucket(ACL='public-read-write',
     Bucket='botflaskproj',
@@ -81,20 +85,22 @@ def bucc():
         'LocationConstraint': 'us-west-2'
     })
     
-       
-@app.route('/encrypt')
-def encrypt():
+
+#encrypt single bucket       
+@app.route('/encryptsinglebucket')
+def encryptsinglebucket():
     cl = boto3.client('s3')
     respo = cl.put_bucket_encryption(
     Bucket='botflaskproj',
     ServerSideEncryptionConfiguration={
     'Rules': [{'ApplyServerSideEncryptionByDefault': {
             'SSEAlgorithm': 'AES256',}},]})
-    return render_template('index.html') 
+    return render_template('encrypt.html') 
 
 
-@app.route('/alencrypt')
-def alencrypt():
+#encrypt all the buckets in an account
+@app.route('/encryptallbuckets')
+def encryptallbuckets():
     s3_resource = boto3.resource('s3')
     cl = boto3.client('s3')
     for bucket in s3_resource.buckets.all():
@@ -106,8 +112,44 @@ def alencrypt():
     return render_template('allencrypt.html') 
         
 
+class ObjSchema(ma.Schema):
+    class Meta:
+        fields = ("Name","Contents")
+
+obj_schema = ObjSchema()
+objj_schema = ObjSchema(many=True)
+
+@app.route('/allfiles')
+def allfiles():
+    client = boto3.client('s3')
+    s3_resource = boto3.resource('s3')
+    response = client.list_buckets()
+    bucketnames = [bucket['Name'] for bucket in response['Buckets']]
+    for x in range(len(bucketnames)):
+        b=bucketnames[x]
+        response = client.list_objects(Bucket=b)
+    result = obj_schema.dump(response)
+    return jsonify(result.data)
+    
+
+@app.route('/all')
+def all():
+    client = boto3.client('s3')
+    s3_resource = boto3.resource('s3')
+    response = client.list_buckets()
+    dict = {}
+    bucketnames = [bucket['Name'] for bucket in response['Buckets']]
+    for x in range(len(bucketnames)):
+        b = bucketnames[x]
+        dict["BucketName"] = [b] 
+        my_bucket = s3_resource.Bucket(b)
+        for file in my_bucket.objects.all():
+            dict.setdefault("Filename", []).append(file.key)
+    return jsonify(dict)
+        
 
 
 if __name__ == '__main__':
     app.run()
+
     
